911576fd018c27512348909d651dd66a115b5dcf
sequina <sccaro89@gmail.com>
2016-06-17 18:32:18 -0500

    display name

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -1,5 +1,15 @@
-// app.controller("ProfileCtrl", function($scope, $location, firebaseURL) {
-// console.log("profile ctrl is here");
+app.controller("ProfileCtrl", function($scope, $location, $timeout,firebaseURL) {
+console.log("profile ctrl is here");
+
+$scope.counter=30;
+var stop;
+
+$scope.start = function(){
+  stop=$timeout(function() {
+    console.log($scope.counter);
+
+  })
+}
 
 
 
@@ -9,4 +19,4 @@
 
 
 
-// });
+});
